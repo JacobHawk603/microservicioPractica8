e192271d76f1f78418cc146d11394556764641c1
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-06-20 21:09:59 -0600

    codigo base

---
+++ conneccion.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const mysql = require('mysql');
+var app = express();
+
+var bodyParser= require('body-parser');
+var con = mysql.createConnection({
+	host:'microserviciopractica.mysql.database.azure.com',
+	user:'Yerry',
+	password:'c0ntr4s3n4p0rm1shu3v0s!!!',
+	database:'microservicio',
+	port: "3306"
+})
+con.connect()
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}))
+	
+
+
+app.use(express.static('public'))
+
+app.post('/agregarUsuario', (req,res)=>{
+	let nombre = req.body.nombre
+	con.query('INSERT INTO usuario values("'+nombre+'")', (err, respuesta, fields)=>{
+		if(err) return console.log('ERROR', err);
+		return res.redirect("http://localhost:8080/breakout.html")
+	})
+})
+
+app.get('/getUsuarios', (req, res)=>{
+
+	con.query('SELECT * FROM usuario', (err, respuesta, field)=>{
+		if(err) return console.log('ERROR: ', err);
+
+		var userHTML = ``
+		var i = 0
+		console.log(respuesta)
+		respuesta.forEach(user =>{
+			i++
+			userHTML +=`<tr>
+							<td>${i}</td>
+							<td>${user.nombre}</td>
+						</tr>`
+							
+		})
+		return res.send(`<table>
+							<tr>
+								<th>id </th>
+								<th>Nombre:</th>
+							</tr>
+							${userHTML}
+						</table>`)
+	})
+})
+
+app.post('/eliminarUsusario', (req,res)=>{
+	let nombre = req.body.nombre
+	con.query("DELETE FROM usuario where nombre='"+nombre+"'", (err, respuesta, fields)=>{
+		if(err) return console.log('ERROR', err);
+		return res.send("<h1>'"+nombre+"', quien te bajó a la novia, recibió justicia divina </h1>");
+	})
+})
+
+app.post('/modificarUsuario', (req,res)=>{
+	let nombreInicial = req.body.nombreInicial
+	let nombreFinal = req.body.nombreFinal
+	con.query("update usuario set nombre='"+nombreFinal+"' where nombre='"+nombreInicial+"'", (err, respuesta, fields)=>{
+		if(err) return console.log('ERROR', err);
+		return res.send("<h1> a '"+nombreInicial+"' le ha ocurrido una metamorphosis y ahora es '"+nombreFinal+"'</h1>");
+	})
+})
+
+app.listen(8080, ()=>{
+	console.log('Servidor escuchando en el puesrto 8080 aiuda porfavor')
+})
---
+++ public/js/validaciones.js
@@ -0,0 +1,8 @@
+function validar(nombre){
+		var nom = document.getElementById('name').value;
+
+		for (var i = 0; i < nom.length; i++) {
+			
+		}
+		alert(nom.length);
+}
